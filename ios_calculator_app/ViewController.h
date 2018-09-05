d0a8b7c73fe3a0ab790b6959a5bbc572a89f059b
Guest User <Guest@mac-lab-01s-mac-mini.local>
2018-09-05 11:45:19 +0600

    buttons are attached with view controller

--- ios_calculator_app/ViewController.h
+++ ios_calculator_app/ViewController.h
@@ -9,6 +9,26 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel;
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UIButton *powerButton;
+@property (weak, nonatomic) IBOutlet UIButton *button_1;
+@property (weak, nonatomic) IBOutlet UIButton *button_2;
+@property (weak, nonatomic) IBOutlet UIButton *button_3;
+@property (weak, nonatomic) IBOutlet UIButton *button_4;
+@property (weak, nonatomic) IBOutlet UIButton *button_5;
+@property (weak, nonatomic) IBOutlet UIButton *button_6;
+@property (weak, nonatomic) IBOutlet UIButton *button_7;
+@property (weak, nonatomic) IBOutlet UIButton *button_8;
+@property (weak, nonatomic) IBOutlet UIButton *button_9;
+@property (weak, nonatomic) IBOutlet UIButton *button_0;
+@property (weak, nonatomic) IBOutlet UIButton *button_multiplication;
+@property (weak, nonatomic) IBOutlet UIButton *button_minus;
+@property (weak, nonatomic) IBOutlet UIButton *button_plus;
+@property (weak, nonatomic) IBOutlet UIButton *button_equals;
+@property (weak, nonatomic) IBOutlet UIButton *button_pi;
+@property (weak, nonatomic) IBOutlet UIButton *button_division;
+@property (weak, nonatomic) IBOutlet UIButton *button_decimalNotation;
 
 
 @end
